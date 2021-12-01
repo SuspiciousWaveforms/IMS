0788c4cb9501253c13b674adef35fa94abb498f7
saimun <saimunwan@gmail.com>
2021-12-01 11:12:40 +0000

    Create databases in SQL.

--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -10,3 +10,25 @@ CREATE TABLE IF NOT EXISTS `ims`.`customers` (
     `surname` VARCHAR(40) DEFAULT NULL,
     PRIMARY KEY (`id`)
 );
+
+CREATE TABLE IF NOT EXISTS `ims`.`orders` (
+    `order_id` INT(11) NOT NULL AUTO_INCREMENT,
+    `customer_id` INT(11) DEFAULT NULL,
+    PRIMARY KEY (`order_id`),
+    FOREIGN KEY (`customer_id`) REFERENCES `customers`(`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `ims`.`orders_items` (
+    `orders_items_id` INT(11) NOT NULL AUTO_INCREMENT,
+    `order_id` INT(11) NOT NULL,
+    `item_id` INT(11) DEFAULT NULL,
+    PRIMARY KEY (`orders_items_id`),
+    FOREIGN KEY (`order_id`) REFERENCES `orders`(`order_id`),
+    FOREIGN KEY (`item_id`) REFERENCES `items`(`item_id`)
+);
+
+CREATE TABLE IF NOT EXISTS `ims`.`items` (
+    `item_id` INT(11) NOT NULL AUTO_INCREMENT,
+    `item_name` INT(11) DEFAULT NULL,
+    PRIMARY KEY (`item_id`)
+);
